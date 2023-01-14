6a44e706fcafade1f0f62bff07ca648d39205092
Antoniusdion <antoniusdion23@gmail.com>
2023-01-14 09:34:15 +0700

    update index

---
+++ script.js
@@ -0,0 +1,12 @@
+var i=0, Text;
+text = 'Selamat datang di portfolio saya';
+
+function mengetik()
+{
+    if(i < Text.length){
+        document.getElementById('')
+        i++;
+        setTimeout(mengetik, 80)
+    }
+}
+mengetik();
