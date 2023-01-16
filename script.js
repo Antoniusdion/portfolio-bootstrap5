c745b463bccb19ed145b0980c8a9ed9ed57657aa
Antoniusdion <antoniusdion23@gmail.com>
2023-01-16 23:01:44 +0700

    update index

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ function mengetik(){
     }
 }
 
+var yakin = confirm("Apakah anda yakin akan mengunjungi situs lain ?");
+
+    if (yakin) {
+         window.location = "https://www.google.com/";
+} 
+
+var ulangi = confirm("Apakah anda mau mengulang?");
+var counter = 0;
+
+while(ulangi){
+    counter++;
+    ulangi = confirm("Apakah anda mau mengulang?");
+}
+
+var pertanyaan = confirm("Apakah kamu Ingin tahu sesuatu?")
+
